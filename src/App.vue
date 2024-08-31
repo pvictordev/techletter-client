87f4b5289ac67185543fae2585b78cec9b2365c2
Victor Purice <victor.purice@ulbsibiu.ro>
2024-08-31 16:17:40 +0300

    modified boilerplate code

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@ import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
 </script>
 
-<template>
+<!-- <template>
   <header>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
@@ -82,4 +82,4 @@ nav a:first-of-type {
     margin-top: 1rem;
   }
 }
-</style>
+</style> -->
--- src/views/AuthorView.vue
+++ src/views/AuthorView.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="Author">
+    <h1>This is an author page</h1>
   </div>
 </template>
 
