ef15cf710c7f3382e330cf295424c0c9683ac225
Victor Purice <victor.purice@ulbsibiu.ro>
2024-08-31 18:37:10 +0300

    updated App.vue

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,6 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-  root page
   <nav>
     <RouterLink to="/">Home</RouterLink>
     <RouterLink to="/about">About</RouterLink>
