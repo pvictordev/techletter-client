3dd3f67f1497cd95cbaa164752f78c2c35568ac5
Victor Purice <victor.purice@ulbsibiu.ro>
2024-08-31 18:33:27 +0300

    removed boilerplate code

--- src/views/AuthorView.vue
+++ src/views/AuthorView.vue
@@ -1,15 +1,5 @@
-<template>
-  <div class="Author">
-    <h1>This is an author page</h1>
-  </div>
-</template>
+<script setup lang="ts"></script>
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
-</style>
+<template>
+  <main>author page</main>
+</template>
