2ce6b67d63ba9438b21c5ea26e6037c5b995ed47
Victor Purice <victor.purice@ulbsibiu.ro>
2024-09-02 00:12:06 +0300

    fixed primevue initialization bug

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import PrimeVue from 'primevue/config'
+import Aura from '@primevue/themes/aura'
 
 import App from './App.vue'
 import router from './router'
@@ -12,7 +13,11 @@ const app = createApp(App)
 app.use(createPinia())
 app.use(PrimeVue, {
   theme: {
-    unstyled: true
+    preset: Aura,
+    options: {
+      prefix: 'p',
+      darkModeSelector: ''
+    }
   }
 })
 app.use(router)
